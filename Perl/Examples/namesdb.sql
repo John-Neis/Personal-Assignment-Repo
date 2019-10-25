479226e7fe0f5af272be3ac27bdb6152be235fff
John-Neis <47619699+John-Neis@users.noreply.github.com>
2019-10-24 19:46:30 -0500

    Added most recent database file namesdb.sql

--- Perl/Examples/namesdb.sql
+++ Perl/Examples/namesdb.sql
@@ -16,6 +16,80 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `classes`
+--
+
+DROP TABLE IF EXISTS `classes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `classes` (
+  `classID` int(11) NOT NULL AUTO_INCREMENT,
+  `classNumber` varchar(3) NOT NULL,
+  `className` varchar(50) NOT NULL,
+  PRIMARY KEY (`classID`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `classes`
+--
+
+LOCK TABLES `classes` WRITE;
+/*!40000 ALTER TABLE `classes` DISABLE KEYS */;
+INSERT INTO `classes` VALUES (1,'365','Programming Languages'),(2,'260','Scripting Languages'),(3,'161','Computer Science II'),(5,'160','Computer Science I'),(6,'242','Data Structures'),(7,'399','Android Programming');
+/*!40000 ALTER TABLE `classes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `classestaken`
+--
+
+DROP TABLE IF EXISTS `classestaken`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `classestaken` (
+  `studentID` int(11) NOT NULL,
+  `classID` int(11) NOT NULL,
+  PRIMARY KEY (`studentID`,`classID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `classestaken`
+--
+
+LOCK TABLES `classestaken` WRITE;
+/*!40000 ALTER TABLE `classestaken` DISABLE KEYS */;
+INSERT INTO `classestaken` VALUES (1,1),(1,2),(1,3),(1,5),(2,2),(2,3),(3,1),(3,3),(4,1),(5,0),(5,1),(5,2),(5,3),(5,7);
+/*!40000 ALTER TABLE `classestaken` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `students` (
+  `studentID` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(25) NOT NULL,
+  `lastName` varchar(25) NOT NULL,
+  PRIMARY KEY (`studentID`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (1,'Tom','Stokke'),(2,'Bob','Smith'),(3,'Kent','Johnson'),(4,'Fred','Olson'),(5,'David','Sexter');
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tblnames`
 --
 
@@ -30,7 +104,7 @@ CREATE TABLE `tblnames` (
   `State` varchar(50) DEFAULT NULL,
   UNIQUE KEY `NameID` (`NameID`),
   KEY `PrimaryKey` (`LastName`,`FirstName`,`NameID`)
-) ENGINE=MyISAM AUTO_INCREMENT=1332 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=1336 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +113,7 @@ CREATE TABLE `tblnames` (
 
 LOCK TABLES `tblnames` WRITE;
 /*!40000 ALTER TABLE `tblnames` DISABLE KEYS */;
-INSERT INTO `tblnames` VALUES (2,'Backman','Nels','Fargo','ND'),(4,'Bennefeld','Greg','Devils Lake','ND'),(15,'Heidt','Brian','Fargo','ND'),(24,'Corimer','Travis','GF AFB','ND'),(25,'Duppong','Chad','Minneapolis','MN'),(26,'Dukart','Al','EGF','MN'),(250,'Olson','Chris','Boston','MA'),(29,'Anderson','Calvin','Crookston','MN'),(52,'Brady','Robin','Fargo','ND'),(56,'Cwikla','Dan','Grand Forks','ND'),(89,'Collins','Paul','El Paso','NM'),(90,'Hilliard','Wesley','Langby','MN'),(105,'Eder','Jason','Grand Forks','ND'),(111,'Kidder','David','Fargo','ND'),(115,'Hinsz','Bryan','Bismarck','ND'),(116,'Hollands','Kelly','Crookston','MN'),(249,'Pinyerd','Clay','Colorado Springs','CO'),(122,'Kaiser','Donnie','Woodbury','MN'),(240,'Dupree','Matt','Thief River Falls','MN'),(55,'Christensen','Guy','Fargo','ND'),(1330,'O\'Neil','Tom','Somewhere in ','MN'),(1329,'Stokke','Tom','ND','BC'),(1328,'Nelson','Jeffrey','Williston','ND'),(1331,'Jackson','Andie','Princeton','NJ');
+INSERT INTO `tblnames` VALUES (2,'Backman','Nels','Fargo','ND'),(4,'Bennefeld','Greg','Devils Lake','ND'),(15,'Heidt','Brian','Fargo','ND'),(24,'Corimer','Travis','GF AFB','ND'),(25,'Duppong','Chad','Minneapolis','MN'),(26,'Dukart','Al','EGF','MN'),(250,'Olson','Chris','Boston','MA'),(29,'Anderson','Calvin','Crookston','MN'),(52,'Brady','Robin','Fargo','ND'),(56,'Cwikla','Dan','Grand Forks','ND'),(89,'Collins','Paul','El Paso','NM'),(90,'Hilliard','Wesley','Langby','MN'),(105,'Eder','Jason','Grand Forks','ND'),(111,'Kidder','David','Fargo','ND'),(115,'Hinsz','Bryan','Bismarck','ND'),(116,'Hollands','Kelly','Crookston','MN'),(249,'Pinyerd','Clay','Colorado Springs','CO'),(122,'Kaiser','Donnie','Woodbury','MN'),(240,'Dupree','Matt','Thief River Falls','MN'),(55,'Christensen','Guy','Fargo','ND'),(1330,'O\'Neil','Tom','Somewhere in ','MN'),(1329,'Stokke','Tom','ND','BC'),(1328,'Nelson','Jeffrey','Williston','ND'),(1331,'Jackson','Andie','Princeton','NJ'),(1332,'H','Daniel','China','IN'),(1333,'Baurceanu','Daniel','Minneapolis','MN');
 /*!40000 ALTER TABLE `tblnames` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,4 +153,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-10-14 16:02:34
+-- Dump completed on 2019-10-24 20:38:34
