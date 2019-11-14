60561c434a18540b0ffab5a4013d28848a55186d
John-Neis <47619699+John-Neis@users.noreply.github.com>
2019-11-13 20:41:13 -0600

    Added Airplane.cs to ATCDisplay. Implemented info readout as well as moving plane icons on the display itself

---
+++ ATCDisplay/ATCDisplay/ATCDisplay/Airplane.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ATCDisplay
+{
+    class Airplane
+    {
+        private int posX;
+        private int posY;
+        private int orgX;
+        private int orgY;
+        private int posAlt;
+        private int vectX;
+        private int vectY;
+        private int speed;
+        private string dest;
+        private string callSign;
+        private Random rand;
+
+        public Airplane(int posX, int posY, int posAlt, int vectX, int vectY, int speed, string dest, string callSign)
+        {
+            rand = new Random();
+            this.posX     = posX;
+            orgX = posX;
+            this.posY     = posY;
+            orgY = posY;
+            this.posAlt   = posAlt;
+            this.vectX    = vectX;
+            this.vectY    = vectY;
+            this.speed    = speed;
+            this.dest     = dest;
+            this.callSign = callSign;
+        }
+
+        public Airplane() // This constructor needs reworking. Do not use.
+        {
+            string[] randDest = { "KGFK", "KMSP", "CYWG", "KDEN"};
+            rand = new Random();
+            
+            this.posX     = (int)(295 * Math.Cos(rand.Next(0, (int)Math.PI * 2)));
+            orgX = posX;
+            this.posY     = (int)(295 * Math.Sin(rand.Next(0, (int)Math.PI * 2)));
+            orgY = posY;
+            this.posAlt   = rand.Next(1600, 35000);
+            this.vectX    = rand.Next(-20, 20);
+            this.vectY    = rand.Next(-20, 20);
+            this.speed    = (int)Math.Sqrt(Math.Pow(this.vectX, 2) + Math.Pow(this.vectY, 2));
+            this.dest     = randDest[rand.Next(0, 3)];
+            this.callSign = "" + (char)rand.Next(65, 90) + (char)rand.Next(65, 90) + rand.Next(100, 999);
+        }
+
+        public void show(PaintEventArgs e, Pen p)
+        {
+            bool showCondition = Math.Sqrt(Math.Pow(posX, 2) + Math.Pow(posY, 2)) <= 300;
+            if(showCondition)
+            {
+                e.Graphics.DrawLine(p, posX - 5, posY - 5, posX + 5, posY + 5);
+                e.Graphics.DrawLine(p, posX - 5, posY + 5, posX + 5, posY - 5);
+                e.Graphics.DrawString(callSign, new Font("Courier New", 5), new SolidBrush(Color.White), posX - 5, posY + 7);
+            } else
+            {
+                posX = orgX;
+                posY = orgY;
+                double rad = Math.PI * rand.Next(0, 359) / 180.0;
+                vectX = (int)(0.01 * speed * Math.Cos(rad));
+                vectY = (int)(0.01 * speed * Math.Sin(rad));
+            }
+        }
+
+        public void update()
+        {
+            posX += vectX;
+            posY += vectY;
+        }
+
+        public void PrintStats(PaintEventArgs e, int x, int y) 
+        {
+            e.Graphics.DrawString(getInfo(), new Font("Courier New", 8), new SolidBrush(Color.FromArgb(0, 150, 0)), x, y);
+        }
+
+        public string getInfo()
+        {
+            if(posAlt >= 18000)
+            {
+                return callSign + "\n" +
+                   "(x, y, alt): (" + posX + ", " + posY + ", FL" + posAlt / 100 + ")\n" +
+                   "<x, y>, speed: <" + vectX + ", " + vectY + ">, " + speed + "kts\n" +
+                   "Destination: " + dest + "\n";
+            }
+            else
+            {
+                return callSign + "\n" +
+                   "(x, y, alt): (" + posX + ", " + posY + ", " + posAlt + ")\n" +
+                   "<x, y>, speed: <" + vectX + ", " + vectY + ">, " + speed + "kts\n" +
+                   "Destination: " + dest + "\n";
+            }
+            
+        }
+    }
+}
--- ATCDisplay/ATCDisplay/ATCDisplay/Form1.cs
+++ ATCDisplay/ATCDisplay/ATCDisplay/Form1.cs
@@ -12,16 +12,68 @@ namespace ATCDisplay
 {
     public partial class ATCDisp : Form
     {
-        private float angle = 0;
-        private Rectangle WinBorder = new Rectangle(1, 1, 1261, 678);
-        private Rectangle InfoPanel = new Rectangle(680, 0, 680, 678);
-        private SolidBrush brush = new SolidBrush(Color.FromArgb(0, 0, 100));
-        private Pen penThick = new Pen(Color.FromArgb(0, 150, 0), 3);
-        private Pen penThin = new Pen(Color.FromArgb(0, 150, 0));
+        private double angle;
+        private Rectangle WinBorder;
+        private Rectangle InfoPanel;
+        private SolidBrush brush;
+        private Pen penThick;
+        private Pen penThickPink;
+        private Pen penThinPink;
+        private Pen penThinWhite;
+        private Pen penThin;
+        Airplane[] planes;
 
         public ATCDisp()
         {
             InitializeComponent();
+            Random rand = new Random();
+            string[] randDest = {"KDLH", "KMSO", "KMSP", "CYWG", "KDEN" };
+
+            angle = 0;
+            WinBorder = new Rectangle(1, 1, 1261, 678);
+            InfoPanel = new Rectangle(680, 0, 680, 678);
+            brush = new SolidBrush(Color.FromArgb(0, 0, 100));
+            penThick = new Pen(Color.FromArgb(0, 150, 0), 3);
+            penThickPink = new Pen(Color.FromArgb(255, 50, 100), 3);
+            penThinPink = new Pen(Color.FromArgb(255, 50, 100));
+            penThinWhite = new Pen(Color.White);
+            penThin = new Pen(Color.FromArgb(0, 150, 0));
+            planes = new Airplane[5];
+
+            // int posX, int posY, int posAlt, int vectX, int vectY, int speed, string dest, string callSign
+            
+
+            for(int i = 0; i < 5; i++)
+            {
+                double rad = degToRad(rand.Next(0, 359));
+                int posX = (int)(295 * Math.Cos(rad));
+                int posY = (int)(295 * Math.Sin(rad));
+                int posAlt = rand.Next(1, 20) * 1500;
+
+                int vectX;
+                int vectY;
+                int speed;
+                if (posAlt < 10000)
+                {
+                    speed = 200;
+                } else if(posAlt < 20000)
+                {
+                    speed = 250;
+                } else
+                {
+                    speed = 300;
+                }
+
+                rad = degToRad(rand.Next(0, 359));
+                vectX = (int)(0.02 * speed * Math.Cos(rad));
+                vectY = (int)(0.02 * speed * Math.Sin(rad));
+                
+                string dest = randDest[rand.Next(0, randDest.Length - 1)];
+                string callSign = "" + (char)rand.Next(65, 90) + (char)rand.Next(65, 90) + rand.Next(100, 999);
+                
+                planes[i] = new Airplane(posX, posY, posAlt, vectX, vectY, speed, dest, callSign);
+            }
+            
         }
 
         private void AnimTicker_Tick(object sender, EventArgs e)
@@ -44,19 +96,24 @@ namespace ATCDisplay
             
             e.Graphics.DrawRectangle(penThick, WinBorder);
             e.Graphics.DrawLine(penThick, 680, 0, 680, 678);
-            
+            for(int i = 0; i < planes.Length; i++)
+            {
+                planes[i].PrintStats(e, 700, 20 + i * 70);
+            }
+
             // Some of the important numbers we're using:
             //  amp    -> diameter of the airspace circle
             //  alpha  -> a small change in the angle for create a dashed circle
             //  radius -> half of the amplitude i.e. the radius of the outer circle
-            
+
             int amp = this.Width / 2 - 50;
             double alpha = degToRad(5);
             double radius = amp / 2;
+            Console.WriteLine(radius);
 
             // Now, to make drawing the concentric circles where we want a bit easier, translate
             // the origin to a point roughly to the first quarter of the window from the left.
-            e.Graphics.TranslateTransform(50 + amp / 2, 50 + amp / 2);
+            e.Graphics.TranslateTransform(50 + (int)radius, 50 + (int)radius);
             e.Graphics.DrawEllipse(penThin, new Rectangle(-amp / 2, -amp / 2, amp, amp));
             
             // Draws dashed concentric circles. Much easier to count to 360 Degrees than to 2*pi,
@@ -76,6 +133,19 @@ namespace ATCDisplay
                     e.Graphics.DrawLine(penThin, x, y, deltX, deltY);
                 }
             }
+
+            e.Graphics.FillEllipse(new SolidBrush(Color.FromArgb(0, 150, 0)), -5, -5, 10, 10);
+            e.Graphics.DrawLine(penThickPink, -10, 10, -10, -10);
+            e.Graphics.DrawLine(penThinPink, -10, -Height / 2 + 18, -10, Height / 2 - 28);
+            //e.Graphics.DrawLine(penThin, 0, 0, (int)(radius * Math.Cos(angle)), (int)(radius * Math.Sin(angle)));
+
+
+            for (int i = 0; i < planes.Length; i++)
+            {
+                planes[i].show(e, penThinWhite);
+                planes[i].update();
+            }
+            //angle += 0.01;
         }
 
         private double degToRad(double angle)
