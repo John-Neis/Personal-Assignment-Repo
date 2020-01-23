e0eb02ab70756ec74b7d9593d5e57956d9c3154a
JohnNeis <john.neis18@gmail.com>
2020-01-23 13:24:00 -0600

    Created database .sql file

--- Java/MysqlCon.java
+++ Java/MysqlCon.java
@@ -1,27 +1,19 @@
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.Connection;
 
-public class MysqlCon
-{
-    public static void main(String[] args)
-    {
-        try
-        {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = 
-                DriverManager.getConnection("jdbc:mysql://localhost:3306/f19db", 
-                "root", "pass260word");
-            Statement sql = con.createStatement();
-            ResultSet rs = sql.executeQuery("select * from library;");
-            while(rs.next())
-            {
-                System.out.println(rs.getString(1) + "|" + rs.getString(2));
-            }
+public class MysqlCon {
 
-            con.close();
-        }
-        catch(Exception e)
-        {
-            System.err.println(e);
+    private static final String url = "jdbc:mysql://localhost:3306/bankusers?autoReconnect=true&useSSL=false";
+    private static final String user = "root";
+    private static final String password = "pass260word";
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Connection con = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected Successfully");
+        } catch(Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- Python/MySQLconnector.py
+++ Python/MySQLconnector.py
@@ -2,7 +2,22 @@ import mysql.connector
 from mysql.connector import errorcode
 
 try:
-    cnx = mysql.connector.connect(user='root', password='pass260word', host='127.0.0.1', database='f19db')
+    cnx = mysql.connector.connect(
+        user='root', 
+        password='pass260word', 
+        host='127.0.0.1', 
+        database='bankusers'
+    )
+    print("Database Connected")
+    cursor = cnx.cursor()
+    sql = "SELECT * FROM users;"
+
+    cursor.execute(sql)
+    records = cursor.fetchall()
+
+    for record in records:
+        print(record)
+
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("Something is wrong with your login credentials")
